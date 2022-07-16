6105e302a4fa86e190a0e1292f3a84959fa3fbde
Lucasshaw12 <shaw51@hotmail.co.uk>
2022-07-16 14:36:24 +0100

    Functionality to download, unzip and organise the UK CPI data.

--- app/functions.py
+++ app/functions.py
@@ -5,13 +5,15 @@ import requests
 import os
 import datetime
 import csv
+from pathlib import Path
+import zipfile
 
 ##################################################
 #   Gathering and organising raw data
 ##################################################
 
 ##########################################
-# Download raw data for UK housing prices
+# Download and organise raw data for UK housing prices
 ##########################################
 
 
@@ -64,7 +66,7 @@ def organise_uk_housing_data():
     print(f'organised UK housing price data - DONE - time taken = {end_time}\n'.upper())
 
 ##########################################
-# Download raw data for UK electricity prices
+# Download and organise raw data for UK electricity prices
 ##########################################
 
 
@@ -95,7 +97,6 @@ def download_uk_electric_data(raw_csv_data):
 def organise_uk_electric_data():
     # Parse, clean and organise all csv data
 
-    print('organising UK electricity data...'.upper(), end='')
     start_time = datetime.datetime.now()  # Use to time how long the function takes to complete
 
     # Read the raw csv  data
@@ -106,7 +107,6 @@ def organise_uk_electric_data():
             if dict_reader.line_num < 9 or dict_reader.line_num > 42:  # remove irrelevant rows of data from file
                 continue
             csvRows.append(row)
-            # print(row['Date'], row['England (pop)'], row['Wales (pop)'], row['Scotland (pop)'], row['NI (pop)'])
 
     # Write out the csv file to a clean file
     csv_obj = open(os.path.join('../raw data/electricdata', 'cleanukcpielectricindex.csv'), 'w', newline='')
@@ -118,3 +118,99 @@ def organise_uk_electric_data():
 
     end_time = datetime.datetime.now() - start_time
     print(f'organised UK electricity CPI price data - DONE - time taken = {end_time}\n'.upper())
+
+##########################################
+# Download and organise raw data for UK electricity prices
+##########################################
+
+
+def download_uk_cpi_data(raw_csv_data):
+    start_time = datetime.datetime.now()  # Use to time how long the function takes to complete
+
+    res = requests.get(raw_csv_data)
+    res.raise_for_status()
+
+    # create folder to store the data
+    os.makedirs('../raw data/cpi', exist_ok=True)
+
+    # Download the .zip file data
+    raw_data_file = open(os.path.join('../raw data/cpi', 'ukcpi.zip'), 'wb')
+    for chunk in res.iter_content(100000):
+        raw_data_file.write(chunk)
+    raw_data_file.close()
+
+    end_time = datetime.datetime.now() - start_time
+    print(f'downloaded UK Consumer price index (CPI) data - DONE - time taken = {end_time}\n'.upper())
+
+    return raw_csv_data
+
+
+def organise_uk_cpi_data():
+    # Unzip, clean and read the cpi .csv file
+
+    start_time = datetime.datetime.now()  # Use to time how long the function takes to complete
+
+    p = Path('../raw data/cpi/')
+
+    # Unzip the .zip file
+    data_zip = zipfile.ZipFile(p / 'ukcpi.zip')
+    data_zip.extractall(p)
+
+    # Get the .csv file (CPI file only) from the .zip extracted folder
+    for filename in p.glob('API*.csv'):
+        uk_cpi_file = filename
+
+    # Read the csv file
+    csv_rows = []
+    with open(uk_cpi_file) as csv_file:
+        reader_obj = csv.reader(csv_file)
+        for row in reader_obj:
+            if reader_obj.line_num != 87 and reader_obj.line_num != 5:
+                continue  # Skip all other rows besides 5 and 87
+            csv_rows.append(row)
+        csv_file.close()
+
+        # Write out the csv file
+        csv_file_obj = open(os.path.join(p, 'ukcpi.csv'), 'w', newline='')
+        csv_writer = csv.writer(csv_file_obj)
+        for row in csv_rows:
+            csv_writer.writerow(row)
+        csv_file_obj.close()
+
+    end_time = datetime.datetime.now() - start_time
+    print(f'unzipped and organised UK CPI data - DONE - time taken = {end_time}\n'.upper())
+
+
+##########################################
+# Download and organise T212 personal holdings
+##########################################
+
+# def download_t212_data():
+#     pass
+#
+#
+# def organise_t212_data():
+#     pass
+
+
+##################################################
+#   Plotting graphs for csv data
+##################################################
+
+##########################################
+# Graphs for housing data
+##########################################
+
+# TODO
+
+##########################################
+# Graphs for electric data
+##########################################
+
+# TODO
+
+##########################################
+# Graphs for uk cpi data
+##########################################
+
+# TODO
--- app/main.py
+++ app/main.py
@@ -5,28 +5,33 @@ import functions  # custom-made functions.py file to be used as module
 import threading
 import time
 
-print('STARTING PROGRAM... \n')
+print('starting program... \n'.upper())
 
 ##########################################
-# Download raw data
+# Download and organise/clean raw data
 ##########################################
 
-# Raw data url's
+# Raw data URLs
 housing_price_url = 'https://www.ons.gov.uk/generator?uri=/economy/inflationandpriceindices/bulletins/housepriceindex/april2022/ce532115&format=csv'
 electric_price_url = 'https://www.ons.gov.uk/generator?format=csv&uri=/economy/inflationandpriceindices/timeseries/l53e/mm23'
+uk_cpi_url = 'https://api.worldbank.org/v2/en/indicator/FP.CPI.TOTL.ZG?downloadformat=csv'
 
 # Run the functions simultaneously using threading
 download_data_thread_1 = threading.Thread(target=functions.download_uk_electric_data, args=[electric_price_url])
 download_data_thread_2 = threading.Thread(target=functions.download_uk_housing_data, args=[housing_price_url])
+download_data_thread_3 = threading.Thread(target=functions.download_uk_cpi_data, args=[uk_cpi_url])
 download_data_thread_1.start()
 download_data_thread_2.start()
+download_data_thread_3.start()
 
 time.sleep(1)
 
 organise_data_thread_1 = threading.Thread(target=functions.organise_uk_housing_data)
 organise_data_thread_2 = threading.Thread(target=functions.organise_uk_electric_data)
+organise_data_thread_3 = threading.Thread(target=functions.organise_uk_cpi_data)
 organise_data_thread_1.start()
 organise_data_thread_2.start()
+organise_data_thread_3.start()
 
 time.sleep(1)
 print('done.'.upper())
